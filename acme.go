3f39892bed8512cf8b6dc38ffc241d8606b74fda
mdouchement <marc.douchement@gmail.com>
2021-09-09 15:09:12 +0200

    Refactor structure

--- acme.go
+++ acme.go
@@ -28,125 +28,132 @@ var (
 	display bool
 )
 
+type controller struct {
+	display bool
+
+	konf    *koanf.Koanf
+	magic   *certmagic.Config
+	manager *certmagic.ACMEManager
+}
+
 func main() {
+	ctrl := &controller{}
+
 	c := &cobra.Command{
 		Use:     "acme",
 		Short:   fmt.Sprintf("Reads %s configuration file from the current directory", acmefile),
 		Version: fmt.Sprintf("%s - build %.7s @ %s", version, revision, date),
 		Args:    cobra.NoArgs,
+		PersistentPreRunE: func(c *cobra.Command, args []string) error {
+			ctrl.konf = koanf.New(delimiter)
+			return ctrl.konf.Load(file.Provider(acmefile), yaml.Parser())
+		},
 		RunE: func(c *cobra.Command, args []string) error {
-			//
-			// Load
-			//
-
-			konf := koanf.New(delimiter)
-			err := konf.Load(file.Provider(acmefile), yaml.Parser())
-			if err != nil {
+			if err := ctrl.configure(); err != nil {
 				return err
 			}
 
-			//
-			// Configure ACME
-			//
-
-			magic := certmagic.NewDefault()
-			magic.Storage = &certmagic.FileStorage{Path: konf.String("storage")}
-
-			switch kt := certmagic.KeyType(konf.String("key_type")); kt {
-			case certmagic.ED25519:
-				fallthrough
-			case certmagic.P256:
-				fallthrough
-			case certmagic.P384:
-				fallthrough
-			case certmagic.RSA2048:
-				fallthrough
-			case certmagic.RSA4096:
-				fallthrough
-			case certmagic.RSA8192:
-				magic.KeySource = certmagic.StandardKeyGenerator{KeyType: kt}
-			default:
-				return fmt.Errorf("unsupported key_type: %s", kt)
-			}
-
-			magic.OnEvent = func(event string, data interface{}) {
-				log.Printf("Event: %s with data: %v\n", event, data)
-			}
-
-			template := certmagic.ACMEManager{
-				Email:  konf.String("email"),
-				Agreed: konf.Bool("agreed"),
-				CA:     certmagic.LetsEncryptProductionCA,
-				//
-				AltHTTPPort:    certmagic.HTTPChallengePort,
-				AltTLSALPNPort: certmagic.TLSALPNChallengePort,
-				//
-				//
-				// DisableHTTPChallenge: false,
-				DisableTLSALPNChallenge: true,
-			}
-			if konf.Bool("staging") {
-				log.Println("Using staging endpoint")
-				template.CA = certmagic.LetsEncryptStagingCA
-			}
-
-			manager := certmagic.NewACMEManager(magic, template)
-			magic.Issuer = manager
-			magic.Revoker = manager
-
-			//
-			// Process ACME challenge
-			//
-
-			certmagic.CleanStorage(magic.Storage, certmagic.CleanStorageOptions{
-				ExpiredCerts: true,
-			})
-
-			mux := http.NewServeMux()
-			mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-				fmt.Fprintf(w, "Lookit my cool website over HTTPS!")
-			})
-
-			log.Println("Staring server for domains:", konf.Strings("domains"))
-			go func() {
-				err = http.ListenAndServe(":80", manager.HTTPChallengeHandler(mux))
-				log.Fatal(err)
-			}()
-
-			err = magic.ManageSync(konf.Strings("domains"))
-			if err != nil {
+			if err := ctrl.challenge(); err != nil {
 				return err
 			}
 
-			if display {
-				displayKeys(konf, manager)
+			if !display {
+				return nil
 			}
-			return nil
+			return ctrl.displayKeys()
 		},
 	}
-	c.Flags().BoolVarP(&display, "display-certificates", "", false, "Display on STDOUT the generated certificates")
+	c.Flags().BoolVarP(&ctrl.display, "display-certificates", "", false, "Display on STDOUT the generated certificates")
 
 	if err := c.Execute(); err != nil {
 		log.Fatal(err)
 	}
 }
 
-func displayKeys(konf *koanf.Koanf, manager *certmagic.ACMEManager) {
-	ca, err := url.Parse(manager.CA)
-	if err != nil {
-		log.Fatal(err)
+func (c *controller) configure() error {
+	c.magic = certmagic.NewDefault()
+	c.magic.Storage = &certmagic.FileStorage{Path: c.konf.String("storage")}
+
+	switch kt := certmagic.KeyType(c.konf.String("key_type")); kt {
+	case certmagic.ED25519:
+		fallthrough
+	case certmagic.P256:
+		fallthrough
+	case certmagic.P384:
+		fallthrough
+	case certmagic.RSA2048:
+		fallthrough
+	case certmagic.RSA4096:
+		fallthrough
+	case certmagic.RSA8192:
+		c.magic.KeySource = certmagic.StandardKeyGenerator{KeyType: kt}
+	default:
+		return fmt.Errorf("unsupported key_type: %s", kt)
 	}
 
-	basedir := filepath.Join(konf.String("storage"), "certificates", ca.Hostname()+"-directory")
+	c.magic.OnEvent = func(event string, data interface{}) {
+		log.Printf("Event: %s with data: %v\n", event, data)
+	}
+
+	template := certmagic.ACMEManager{
+		Email:  c.konf.String("email"),
+		Agreed: c.konf.Bool("agreed"),
+		CA:     certmagic.LetsEncryptProductionCA,
+		//
+		AltHTTPPort:    certmagic.HTTPChallengePort,
+		AltTLSALPNPort: certmagic.TLSALPNChallengePort,
+		//
+		//
+		// DisableHTTPChallenge: false,
+		DisableTLSALPNChallenge: true,
+	}
+	if c.konf.Bool("staging") {
+		log.Println("Using staging endpoint")
+		template.CA = certmagic.LetsEncryptStagingCA
+	}
+
+	c.manager = certmagic.NewACMEManager(c.magic, template)
+	c.magic.Issuer = c.manager
+	c.magic.Revoker = c.manager
+
+	return nil
+}
+
+func (c *controller) challenge() error {
+	certmagic.CleanStorage(c.magic.Storage, certmagic.CleanStorageOptions{
+		ExpiredCerts: true,
+	})
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		fmt.Fprintf(w, "Lookit my cool website over HTTPS!")
+	})
+
+	log.Println("Staring server for domains:", c.konf.Strings("domains"))
+	go func() {
+		err := http.ListenAndServe(":80", c.manager.HTTPChallengeHandler(mux))
+		log.Fatal(err)
+	}()
+
+	return c.magic.ManageSync(c.konf.Strings("domains"))
+}
+
+func (c *controller) displayKeys() error {
+	ca, err := url.Parse(c.manager.CA)
+	if err != nil {
+		return err
+	}
+
+	basedir := filepath.Join(c.konf.String("storage"), "certificates", ca.Hostname()+"-directory")
 	log.Println("Base directory:", basedir)
 
-	for _, domain := range konf.Strings("domains") {
+	for _, domain := range c.konf.Strings("domains") {
 		filename := domain + ".key"
 		fmt.Printf("=> %s\n\n", filename)
 
 		payload, err := ioutil.ReadFile(filepath.Join(basedir, domain, filename))
 		if err != nil {
-			log.Fatal(err)
+			return err
 		}
 		fmt.Println(string(payload))
 
@@ -155,8 +162,9 @@ func displayKeys(konf *koanf.Koanf, manager *certmagic.ACMEManager) {
 
 		payload, err = ioutil.ReadFile(filepath.Join(basedir, domain, filename))
 		if err != nil {
-			log.Fatal(err)
+			return err
 		}
 		fmt.Println(string(payload))
 	}
+	return nil
 }
--- go.mod
+++ go.mod
@@ -1,16 +1,13 @@
 module github.com/mdouchement/acme
 
-go 1.13
+go 1.16
 
 require (
 	github.com/caddyserver/certmagic v0.10.13
 	github.com/cenkalti/backoff/v4 v4.0.2 // indirect
-	github.com/davecgh/go-spew v1.1.1
 	github.com/klauspost/cpuid v1.2.4 // indirect
 	github.com/knadh/koanf v0.5.0
-	github.com/mholt/certmagic v0.7.5
 	github.com/miekg/dns v1.1.29 // indirect
-	github.com/sanity-io/litter v1.2.0
 	github.com/spf13/cobra v0.0.5
 	golang.org/x/crypto v0.0.0-20200510223506-06a226fb4e37 // indirect
 	golang.org/x/net v0.0.0-20200513185701-a91f0712d120 // indirect
